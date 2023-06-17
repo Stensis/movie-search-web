b773fe88c5b36520d7b0e78bfba22a6d654298b8
Stensis <irenenjuguna98@gmail.com>
2023-06-17 15:35:46 +0300

    additional information on button click to show more details

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // index.js
-
-// ...
-
 // Define constants
 const apiKey = "1d67cb07";
 
@@ -23,9 +20,6 @@ searchForm.addEventListener("submit", function (event) {
     fetchMovieData(searchTerm);
   }
 });
-
-// ...
-
 // Fetch movie data from the OMDB API
 async function fetchMovieData(searchTerm) {
   // Construct the API URL with the search term
@@ -54,9 +48,6 @@ async function fetchMovieData(searchTerm) {
     displayErrorMessage("An error occurred while fetching data.");
   }
 }
-
-// ...
-
 // Filter movies based on the search term
 function filterMoviesByTitle(movies, searchTerm) {
   const filteredMovies = movies.filter((movie) => {
@@ -79,13 +70,61 @@ function displayMovieResults(movies) {
     const movieItem = document.createElement("div");
     movieItem.classList.add("movie-item");
 
-    // ...
-
     // Append the movie item to the movie list container
     movieList.appendChild(movieItem);
   });
-
-  // ...
 }
+// Show additional information when a "Read More" button is clicked
+async function showAdditionalInfo(event) {
+  const button = event.target;
+  const movieId = button.getAttribute("data-id");
+  const additionalInfo = button.parentNode.querySelector(".additional-info");
 
-// ...
+  if (additionalInfo.style.display === "none") {
+    try {
+      // Construct the API URL for a specific movie
+      const apiMovieUrl = `http://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`;
+
+      // Make the API request
+      const response = await fetch(apiMovieUrl);
+      const data = await response.json();
+
+      if (data.Response === "True") {
+        // Extract relevant data from the response
+        const { Plot, Genre, Actors } = data;
+
+        // Display the additional information
+        additionalInfo.innerHTML = `
+            <div style="text-align: left">
+              <p style="font-size: 20px;color:#e5e5e5">
+                <strong style="color:#ef233c">Plot:</strong>
+                ${Plot}
+              </p>
+              <p style="font-size: 20px;color:#e5e5e5">
+                <strong style="color:#ef233c">Genre:</strong>
+                ${Genre}
+              </p>
+              <p style="font-size: 20px;color:#e5e5e5">
+                <strong style="color:#ef233c">Actors:</strong>
+                ${Actors}
+              </p>
+            </div>
+          `;
+
+        // Update the display and text content of the button
+        additionalInfo.style.display = "block";
+        button.textContent = "Go Back";
+      } else {
+        // Display an error message if the response is not successful
+        displayErrorMessage(data.Error);
+      }
+    } catch (error) {
+      // Display an error message if an error occurs during the API request
+      displayErrorMessage("An error occurred while fetching movie data.");
+    }
+  } else {
+    // Hide the additional information and revert the button text
+    additionalInfo.style.display = "none";
+    button.textContent = "View details";
+  }
+}
